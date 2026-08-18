7f6a05204ce4a0ec78c9931971050a5c3c83af2f
Karthik G. Appiah <122487930+karthikgappiah@users.noreply.github.com>
2026-08-18 15:04:23 -0700

    build: add better auth (oauth only, no db)

---
+++ src/library/auth/auth-client.ts
@@ -0,0 +1,3 @@
+import { createAuthClient } from "better-auth/react";
+
+export const auth_client = createAuthClient();
--- src/library/env/server.ts
+++ src/library/env/server.ts
@@ -1,11 +1,10 @@
 import { createEnv } from "@t3-oss/env-nextjs";
 import { vercel } from "@t3-oss/env-nextjs/presets-zod";
-import * as z from "zod";
 
 export const env = createEnv({
   extends: [vercel()],
-  server: {
-    DATABASE_URL: z.url(),
-  },
+  server: {},
+  emptyStringAsUndefined: true,
+  skipValidation: process.env.SKIP_ENV_VALIDATION === "1",
   experimental__runtimeEnv: process.env,
 });
