d28ec0591d9dfd6b4eb3400ef23a75d0c68122fb
Karthik G. Appiah <122487930+karthikgappiah@users.noreply.github.com>
2026-08-19 11:06:16 -0700

    feat: scaffold nav header

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,11 +1,22 @@
+import { SpotifyLogoIcon } from "@phosphor-icons/react/dist/ssr";
 import { Button } from "@/src/components/ui/button";
 import { ModeToggle } from "@/src/components/ui/mode-toggle";
 
 export default function LandingPage() {
   return (
-    <main className="flex grow items-center justify-center gap-2">
-      <Button>TODO</Button>
-      <ModeToggle />
+    <main className="grow">
+      <header className="flex items-center justify-between p-4">
+        <div className="flex items-center space-x-1">
+          <SpotifyLogoIcon size={32} />
+          <h1 className="text-xl">Spotify</h1>
+        </div>
+        <div className="flex items-center space-x-2">
+          <ModeToggle />
+          <Button variant="outline" type="button">
+            <p>Sign In</p>
+          </Button>
+        </div>
+      </header>
     </main>
   );
 }
