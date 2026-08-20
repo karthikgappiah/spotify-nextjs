1c993a9412ff230b22dbd4b0129d092394bbbc7c
Karthik G. Appiah <122487930+karthikgappiah@users.noreply.github.com>
2026-08-19 21:24:38 -0700

    feat: add liked music header

--- src/app/(pages)/home/liked/page.tsx
+++ src/app/(pages)/home/liked/page.tsx
@@ -1,3 +1,20 @@
+import { ThumbsUpIcon } from "@phosphor-icons/react/dist/ssr";
+
 export default function LikedMusicPage() {
-  return <h2 className="flex grow items-center justify-center">Liked Music</h2>;
+  return (
+    <>
+      <section className="flex items-end space-x-4">
+        <div className="flex size-64 items-center justify-center rounded-4xl bg-muted">
+          <ThumbsUpIcon size={64} weight="duotone" />
+        </div>
+        <div>
+          <h2 className="font-semibold text-4xl">Liked Music</h2>
+          <p className="text-muted-foreground text-sm">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
+        </div>
+      </section>
+      <section>{/* DATA TABLE */}</section>
+    </>
+  );
 }
