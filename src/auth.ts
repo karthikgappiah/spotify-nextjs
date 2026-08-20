ff4865c527c78c7975417b40a5034f1f5ead6dde
Karthik G. Appiah <122487930+karthikgappiah@users.noreply.github.com>
2026-08-20 00:22:16 -0700

    feat: add database session persistence

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,9 @@
 import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { nextCookies } from "better-auth/next-js";
+import { account, session, user, verification } from "@/src/database/models";
 import { env } from "@/src/library/env/server";
+import { db } from "./database";
 
 const spotify_scopes = ["user-read-private", "user-library-read"];
 
@@ -13,6 +16,10 @@ export const auth = betterAuth({
       scope: spotify_scopes,
     },
   },
+  database: drizzleAdapter(db, {
+    provider: "pg", // Database Options: "pg", "mysql", "sqlite".
+    schema: { user, session, account, verification },
+  }),
   account: {
     encryptOAuthTokens: true,
   },
